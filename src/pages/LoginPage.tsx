b8a89e28b4e9325c04fff53fde62000f12aa6bba
Saai Aravindh Raja <saaiaravindhraja@gmail.com>
2025-08-21 15:56:12 +0800

    Style login and signup forms

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+import { TextField } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
     return (
-        <div>
-            <h1>Login Page</h1>
-        </div>
+        <Container className="mt-5">
+            <h1>Login to CodeLingo</h1>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <TextField id="email" label="Email" variant="outlined" fullWidth />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <TextField id="password" label="Password" type="password" variant="outlined" fullWidth />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+            </Form>
+            <div className="mt-3">
+                <Link to="/signup">Don't have an account? Sign Up</Link>
+            </div>
+        </Container>
     );
 };
 
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+import { TextField } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const SignupPage = () => {
     return (
-        <div>
-            <h1>Signup Page</h1>
-        </div>
+        <Container className="mt-5">
+            <h1>Create your CodeLingo Account</h1>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicUsername">
+                    <TextField id="username" label="Username" variant="outlined" fullWidth />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <TextField id="email" label="Email" variant="outlined" fullWidth />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <TextField id="password" label="Password" type="password" variant="outlined" fullWidth />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Sign Up
+                </Button>
+            </Form>
+            <div className="mt-3">
+                <Link to="/login">Already have an account? Login</Link>
+            </div>
+        </Container>
     );
 };
 
